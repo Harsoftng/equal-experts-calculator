c5b9c92fd71ebf8301c625388d44523f848342cc
Ibrahim Haruna <harsoftng@gmail.com>
2021-12-20 14:46:10 +0100

    Modified the App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={(f) => f} label={5} value={5} size={2} />
+      <Button onClick={(f) => f} label={5} value={5} size={1} action={true} />
     </div>
   );
 }
